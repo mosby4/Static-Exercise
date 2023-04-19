08a92c2e3cab437205ee879589dac1f6af12b9ee
Mosby4 <rufus.hamby@gmial.com>
2023-04-19 11:06:41 -0500

    Exercise Complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(TempConverter.FarenheitToCelsius(90));
+            Console.WriteLine(TempConverter.CelsiusToFarenheit(23));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		/*public static TempConverter()
+		{
+		}*/
+
+		public static double FarenheitToCelsius(double input1)
+		{
+			
+			return (input1 -32) / 1.8;
+        }
+
+        //
+        public static double CelsiusToFarenheit(double input1)
+		{
+            //°F =°C * 1.8000+ 32.00
+            return (input1 * 1.8) +32;
+
+        }
+    }
+}
+
